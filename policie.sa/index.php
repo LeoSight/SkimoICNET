24c34d292bc46418cf485940814690729ce93ab2
Rataj <rataj@fabulasoft.cz>
2017-07-06 15:08:28 +0200

    Oprava chyby v HTML syntaxi

--- policie.sa/index.php
+++ policie.sa/index.php
@@ -4,7 +4,7 @@
 	<p>Policie San Andreas nedávno pořádala nábor a přijala několik nových policistů. Doufáme že se jim při jejich práci bude dařit a přejeme jim hodně štěstí s udržování pořádku v našem městě.</p>
 	<h2>Informace</h2>
 	<p>Velitel policie: Edan Murrough<br />Zástupce: Vladimir Kozlov</p>
-	<h3>P�idej se i ty!</h3
+	<h3>Přidej se i ty!</h3>
 	<img src="http://i.imgur.com/NBoGP92.png"/>
 
 <?php include('includes/footer.php'); ?>	
