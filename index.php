76a3ef82e62b00553867a565bc64127dd9305b20
Rataj <rataj@fabulasoft.cz>
2017-07-06 14:25:53 +0200

    Přidán bootstrap a jquery, na které můžete odkazovat z IC webů

---
+++ index.php
@@ -0,0 +1,9 @@
+<html lang="cs">
+	<head>
+		<title>SkimoICNET</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<p>Weby v tomto adresáři jsou čistě herní! Informace, které lze naleznete neodpovídají realitě.</p>
+	</body>
+</html>
