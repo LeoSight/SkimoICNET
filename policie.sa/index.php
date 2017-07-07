50f71a3a06a37d713fda76031a1c11ac5c81964b
pavot <pavelvotypka02@gmail.com>
2017-07-07 14:11:21 +0200

    Přidání článku o kamerách

--- policie.sa/index.php
+++ policie.sa/index.php
@@ -4,7 +4,10 @@
 	<p>Policie San Andreas nedÃ¡vno poÅ™Ã¡dala nÃ¡bor a pÅ™ijala nÄ›kolik novÃ½ch policistÅ¯. DoufÃ¡me Å¾e se jim pÅ™i jejich prÃ¡ci bude daÅ™it a pÅ™ejeme jim hodnÄ› Å¡tÄ›stÃ­ s udrÅ¾ovÃ¡nÃ­ poÅ™Ã¡dku v naÅ¡em mÄ›stÄ›.</p>
 	<h2>Informace</h2>
 	<p>Velitel policie: Edan Murrough<br />ZÃ¡stupce: Vladimir Kozlov</p>
-	<h3>Pøidej se i ty!</h3
+	<h3> Vláda a TSI </h3>
+	<p> Dne 6.7 2017 se zaèalo pracovat na kamerovém systému pro Policii San Andreas. Kamery jsou rozmístìny rùznì po celém San Andreas, nejvíce jich mùete spatøit v Los Santos. Dìkujeme vládì za zajištìní penìz a TSI za zprostøedkování tìchto kamer.
+	<img src="http://i.imgur.com/FfD9uDH.png"/>
+	<h4>Pøidej se i ty!</h4>
 	<img src="http://i.imgur.com/NBoGP92.png"/>
 
 <?php include('includes/footer.php'); ?>	
