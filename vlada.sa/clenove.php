e302dd5dc60daf5974e85c65c285002bfe6cf7b7
Testeuros <Testeur1998@gmail.com>
2017-07-14 18:27:01 +0200

    14.7clenove.php
    Upravený seznam členů.

--- vlada.sa/clenove.php
+++ vlada.sa/clenove.php
@@ -6,6 +6,18 @@
 			
 			<h3>Guvernér</h3>
 			<p>Johnny Ward</p>
+		
+			<h3>Starosta města Los Santos</h3>
+			<p>Alan Winters</p>
+		
+			<h3>Ministr financí</h3>
+			<p>Mike Malls</p>
+		
+			<h3>Ministr vnitra</h3>
+			<p>Owen Clipper</p>
+		
+			<h3>Soudce</h3>
+			<p>Dwayne Douglass</p>
 
 		<div class="stripes"></div>
 	</article>
