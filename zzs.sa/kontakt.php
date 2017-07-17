86fe725c1722312af850c43dfd67ae28eaca0018
SedliX <peter888@seznam.cz>
2017-07-18 00:31:21 +0200

    Update kontakt.php

--- zzs.sa/kontakt.php
+++ zzs.sa/kontakt.php
@@ -3,7 +3,7 @@
 	<h1>Kontakt</h1>
 	<p>Tísňová linka 155</p>
 	<p>Integrovaný záchranný systém 112</p>
-	<p>Ředitel ZZS LS, p.o. - Dr. John Blackwood, M.D. DSc (433187)</p>
+	<p>Ředitel ZZS LS, p.o. - MUDr. John Blackwood, DSc. (433187)</p>
 	<p>Nemocnice All Saint & Výjezdové stanoviště ZZS LS, p.o. na All Saint</p>
 	<p><b>Tísňová linka 155 i 112 funguje neustále a není zpoplatněna!</b></p>
 
