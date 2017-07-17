6c097fe9cee9ba49d80d7ddfc029a1155e6acb76
SedliX <peter888@seznam.cz>
2017-07-17 22:34:13 +0200

    Update index.php

--- zzs.sa/index.php
+++ zzs.sa/index.php
@@ -1,6 +1,10 @@
 <?php $page=1; include('includes/header.php'); ?>
 
+	<h1>Záchranka v LS</h1>
+	<p>Zdravotnická záchranná služba Los Santos, příspěvková organizace vznikla k 2. 7. 2017 odpojením od Zdravotnické záchranné služby San Andreas, která tehdy spravovala celou oblast státu San Andreas. Řízení systému poskytování přednemocniční péče v Los Santos a přilehlém okolí bylo schváleno jednáním Vlády San Andreas ze dne 12. 9. 2016</p>
+	<p>ZZS LS vykonává veškeré úkony v rámci garantované způsobilosti a při dodržování všech předpisů i norem, které upravují technické, hygienické i etické podmínky výkonu poskytování zdravotní péče se specifiky odborné neodkladné péče.</p>
 	<h1>Novinky</h1>
-	<p>A dam fam kl gsdg opkfnadj nfuaf ifjiafnaj nfjf rrg gtdh fjzg fsdg sdhthjnf</p>
-
+	<p>Zdravotnická záchranná služba Los Santos, p.o. vyhlašuje přijímací řízení do studijního programu. Tato informace platí až do odvolání, zájemci kontaktujte ředitele Johna Blackwooda.</p>
+	<p>Studijní program umožňuje všem přihlášeným získat oprávnění k výkonu práce u ZZS LS, p.o. dle vlastního zájmu.</p>
+	
 <?php include('includes/footer.php'); ?>
